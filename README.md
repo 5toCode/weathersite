2a4f5ac0b8da9754be60e8d7f29bdd9e85baa185
mrobertsjr <70207997+mrobertsjr@users.noreply.github.com>
2021-01-03 07:41:28 -0500

    take input from user and show weather details

---
+++ README.md
@@ -0,0 +1,9 @@
+# Weather Project
+```
+Takes zip code as input from user and displays weather for that location
+```
+### Technologies used:
+* Basic HTML, JavaScript
+* NodeJS
+* Express, Body Parser
+* API Authentication to Open Weather
--- app.js
+++ app.js
@@ -1,24 +1,35 @@
 const express = require('express');
 const https = require('https');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.get('/', function(req, res) {
-    const url = 'https://api.openweathermap.org/data/2.5/weather?zip=32461&appid=a797c2972f9898eddc70687f44b96366&units=imperial'
-    https.get(url, function(response) {
-        console.log(response.statusCode);
+app.use(bodyParser.urlencoded({extended: true}));
 
-        response.on('data', function(data) {
-            const weatherData = JSON.parse(data);
-            const temp = weatherData.main.temp;
-            const description = weatherData.weather[0].description;
-            const icon = weatherData.weather[0].icon;
-            const imageURL = 'https://openweathermap.org/img/wn/' + icon + '@2x.png';
-            res.write('<h1>The temperature in Rosemary Beach is ' + temp + ' degrees Fahrenheit.</h1>');
-            res.write('<h2>The weather is currently ' + description + '.</h2>');
-            res.write('<img src=' + imageURL + '>');
-            res.send();
-        });
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/index.html');
+});
+
+
+
+app.post('/', function(req, res) {
+    const zip = req.body.zip;
+    const apiKey = 'a797c2972f9898eddc70687f44b96366';
+    const unit = 'imperial'
+    const url = 'https://api.openweathermap.org/data/2.5/weather?zip=' + zip + '&appid=' + apiKey + '&units=' + unit;
+    https.get(url, function(response) {
+    response.on('data', function(data) {
+        const weatherData = JSON.parse(data);
+        const cityName = weatherData.name;
+        const temp = weatherData.main.temp;
+        const description = weatherData.weather[0].description;
+        const icon = weatherData.weather[0].icon;
+        const imageURL = 'https://openweathermap.org/img/wn/' + icon + '@2x.png';
+        res.write('<h1>The temperature in ' + zip + ' (' + cityName + ') is ' + temp + ' degrees Fahrenheit.</h1>');
+        res.write('<h2>The weather is currently ' + description + '.</h2>');
+        res.write('<img src=' + imageURL + '>');
+        res.send();
+      });
     });
 });
 
